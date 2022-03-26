7878a02e729ecb5662369994bc1e835f5d317b34
maximtrp <maximtrp@gmail.com>
2022-03-26 15:10:35 +0300

    metrics submodule with entropy added

--- src/tmplot/__init__.py
+++ src/tmplot/__init__.py
@@ -3,5 +3,6 @@ from ._distance import *  # noqa: F401, F403
 from ._report import *  # noqa: F401, F403
 from ._stability import *  # noqa: F401, F403
 from ._vis import *  # noqa: F401, F403
+from ._metrics import *  # noqa: F401, F403
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
---
+++ src/tmplot/_metrics.py
@@ -0,0 +1,73 @@
+"""Metrics module"""
+from math import log
+import numpy as np
+
+
+def entropy(phi: np.ndarray):
+    """Renyi entropy calculation routine [1]_.
+
+    Renyi entropy can be used to estimate the optimal number of topics: just fit
+    several models with a different number of topics and choose the number of
+    topics for which the Renyi entropy is the least.
+
+    Parameters
+    ----------
+    phi : np.ndarray
+        Topics vs words probabilities matrix (T x W).
+
+    Returns
+    -------
+    renyi : double
+        Renyi entropy value.
+
+    References
+    ----------
+    .. [1] Koltcov, S. (2018). Application of Rényi and Tsallis entropies to
+           topic modeling optimization. Physica A: Statistical Mechanics and its
+           Applications, 512, 1192-1204.
+
+    Example
+    -------
+    >>> import tmplot as tmp
+    >>> # Preprocessing step
+    >>> # ...
+    >>> # Model fitting step
+    >>> # model = ...
+    >>> # Entropy calculation
+    >>> entropy = tmp.entropy(model.matrix_topics_words_)
+    """
+    # Terms number
+    words_num = phi.shape[1]
+    # Topics number
+    topics_num = phi.shape[0]
+
+    # Setting threshold
+    thresh = 1 / words_num
+
+    # Maximum probability of each word
+    p_max = np.zeros(words_num)
+
+    # Obtaining maximum p value over all topics for each word
+    p_max = np.max(phi, axis=0)
+
+    # Select the probabilities larger than thresh
+    p_max_mask = p_max > thresh
+    word_ratio = p_max_mask.sum()
+    sum_prob = p_max[p_max_mask].sum()
+
+    # Shannon entropy
+    shannon = log(word_ratio / (words_num * topics_num))
+
+    # Internal energy
+    int_energy = -log(sum_prob / topics_num)
+
+    # Free energy
+    free_energy = int_energy - shannon * topics_num
+
+    # Renyi entropy
+    if topics_num == 1:
+        renyi = free_energy / topics_num
+    else:
+        renyi = free_energy / (topics_num-1)
+
+    return renyi
